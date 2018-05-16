521b51d71ede9818888960c9d487625aea1a96e9
Conor Hamilton <conhamil19@gmail.com>
2018-05-16 05:36:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,11 @@ programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
-
+programmer_hash[
+ :yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
 
 	programmer_hash = 
  		{
@@ -76,11 +80,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[
- :yukihiro_matsumoto] = {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
 
     
 end
